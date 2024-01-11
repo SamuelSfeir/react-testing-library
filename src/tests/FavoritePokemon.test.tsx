5a627bdf0b823ccfe16836751e6da22ad4baf4e5
Samuel Sfeir <samuel1808@hotmail.com>
2024-01-11 09:30:32 -0300

    Adiciona requisito quatro

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemon } from '../pages';
 
--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+beforeEach(() => {
+  renderWithRouter(<NotFound />);
+});
+
+describe('Teste o componente <NotFound.js />', () => {
+  // Testa se a página contém um heading h2 com o texto "Page requested not found".
+  it('Se a página contém um heading h2 com o texto Page requested not found', () => {
+    // Obtém o elemento do título usando o atributo `role` e `name`.
+    const title = screen.getByRole('heading', {
+      name: 'Page requested not found',
+      level: 2,
+    });
+    // Verifica se o título está presente no documento.
+    expect(title).toBeInTheDocument();
+  });
+});
