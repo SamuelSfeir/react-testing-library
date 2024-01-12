3e2b70bba7bb2de43a7466e827b1e316256ef8ef
Samuel Sfeir <samuel1808@hotmail.com>
2024-01-12 10:13:47 -0300

    commit

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
@@ -10,6 +9,12 @@ beforeEach(() => {
 
 describe(`Se é exibido o próximo pokémon da lista quando o botão Próximo pokémon
   é clicado`, () => {
+  it('O botão deve conter o texto Próximo pokémon', () => {
+    const button = screen.getByRole('button', { name: 'Próximo Pokémon' });
+
+    expect(button).toBeInTheDocument();
+  });
+
   it('É possível clicar no botão de filtragem All', () => {
     const buttonAll = screen.getByRole('button', { name: /all/i });
 
@@ -17,10 +22,19 @@ describe(`Se é exibido o próximo pokémon da lista quando o botão Próximo po
     userEvent.click(buttonAll);
   });
 });
+
 describe('Teste os botões', () => {
   it('Se tem a quantidade correta de botões', () => {
     const qtdButons = 7;
     const buttons = screen.getAllByTestId('pokemon-type-button');
     expect(buttons.length).toBe(qtdButons);
+  });
+
+  it('Se o texto corresponde ao tipo', () => {
+    const buttons = screen.getByRole('button', { name: /fire/i });
+    userEvent.click(buttons);
+
+    const type = screen.getAllByTestId('pokemon-type');
+    expect(type[0].textContent).toBe('Electric');
   });
 });
